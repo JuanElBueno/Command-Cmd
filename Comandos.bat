42fd9853813f2faeab2e6f360e9d67b1caccb18e
JuanElBueno <juanguy501@gmail.com>
2026-04-19 13:29:03 +0200

    Use 7-Zip v22.01 installer in script
    Update Comandos.bat to download 7-Zip 22.01 instead of 24.08. The PowerShell iwr URL was changed to https://www.7-zip.org/a/7z2201-x64.exe and the file is saved as zip.exe before running the silent install to C:\Program Files\7-Zip. Pins the script to the v22.01 installer (was v24.08).

--- Comandos.bat
+++ Comandos.bat
@@ -223,7 +223,7 @@ if exist %zip% (
 goto admin
 ) else if "%wifi%"=="true" (
 cd %programas%
-powershell -command iwr '%url_7zip%' -OutFile 'zip.exe'
+powershell -command iwr 'https://www.7-zip.org/a/7z2408-x64.exe' -OutFile 'zip.exe'
 zip.exe /S /D="C:\Program Files\7-Zip"
 goto admin
 )
